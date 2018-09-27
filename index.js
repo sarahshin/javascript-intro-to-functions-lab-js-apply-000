b34db5a678a9301c803fe98dd956bff4954f0c3d
Sarah Shin <sarahsujin.shin@gmail.com>
2018-09-27 19:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can\'t hear you!";
   }
+  else if(string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else {
+    return 
+  }
 }
